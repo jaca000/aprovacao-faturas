23c023a66e9a9b7de4131b79898d0e355c202e59
jaca000 <almanso.jose@gmail.com>
2026-03-10 12:22:53 +0000

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,35 @@
+const msalConfig = {
+    auth: {
+        clientId: "COLOCA_AQUI_O_CLIENT_ID",
+        authority: "https://login.microsoftonline.com/common",
+        redirectUri: window.location.origin
+    }
+};
+
+const msalInstance = new msal.PublicClientApplication(msalConfig);
+
+const loginRequest = {
+    scopes: ["User.Read"]
+};
+
+async function iniciarLogin() {
+
+    const contas = msalInstance.getAllAccounts();
+
+    if (contas.length === 0) {
+
+        await msalInstance.loginRedirect(loginRequest);
+
+    } else {
+
+        const utilizador = contas[0];
+
+        console.log("Utilizador autenticado:", utilizador.name);
+        console.log("Email:", utilizador.username);
+
+        sessionStorage.setItem("userName", utilizador.name);
+        sessionStorage.setItem("userEmail", utilizador.username);
+
+    }
+
+}
