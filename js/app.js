b01329a84eb1b1ca3e02a834cee69f19472e1553
jaca000 <almanso.jose@gmail.com>
2026-03-11 14:36:34 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,33 +1,5 @@
-console.log("Aplicação iniciada");
+console.log("App iniciada");
 
-async function iniciarApp() {
-
-    await iniciarLogin();
-
-    const email = sessionStorage.getItem("userEmail");
-
-    if (email) {
-
-        console.log("Utilizador autenticado:", email);
-
-        carregarDashboard();
-
-    }
-
-}
-
-function carregarDashboard(){
-
-    document.getElementById("conteudo").innerHTML = `
-    
-    <h2>Dashboard</h2>
-    
-    <p>Aplicação em construção</p>
-    
-    <button onclick="testarGraph()">Testar ligação Graph</button>
-
-    `;
-
-}
-
-iniciarApp();
+document.getElementById("app").innerHTML = `
+<button onclick="login()">Iniciar sessão Microsoft</button>
+`;
