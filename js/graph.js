46db750ccf3c61e901b7f50bfa91fe09c553ecb6
jaca000 <almanso.jose@gmail.com>
2026-03-11 14:15:57 +0000

    Update graph.js

--- js/graph.js
+++ js/graph.js
@@ -3,7 +3,7 @@ async function getAccessToken() {
     const account = msalInstance.getAllAccounts()[0];
 
     const request = {
-        scopes: ["User.Read", "Sites.ReadWrite.All"],
+        scopes: ["User.Read", "Sites.Read.All"],
         account: account
     };
 
@@ -14,11 +14,33 @@ async function getAccessToken() {
 
     } catch (error) {
 
-        console.log("Token silencioso falhou, pedir novamente...");
+        console.log("Silent token falhou");
 
         const response = await msalInstance.acquireTokenPopup(request);
         return response.accessToken;
 
     }
+
+}
+
+
+async function testarGraph() {
+
+    console.log("A testar ligação ao Microsoft Graph");
+
+    const token = await getAccessToken();
+
+    const resposta = await fetch(
+        "https://graph.microsoft.com/v1.0/me",
+        {
+            headers: {
+                Authorization: "Bearer " + token
+            }
+        }
+    );
+
+    const dados = await resposta.json();
+
+    console.log("Dados do utilizador Graph:", dados);
 
 }
