969fdb99b02432ff0cafab7aa9963a4771500148
jaca000 <almanso.jose@gmail.com>
2026-03-11 14:39:29 +0000

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,37 +1,30 @@
 const msalConfig = {
     auth: {
-        clientId: "81693cb9-9ffb-41d0-b0ff-41dbf29990eb",
+        clientId: "O_TEU_CLIENT_ID",
         authority: "https://login.microsoftonline.com/ee417351-ea90-41e0-9147-5ea6ab38ea49",
-        redirectUri: "https://jaca000.github.io/aprovacao-faturas/"
+        redirectUri: "https://jaca000.github.io/"
     }
 };
 
 const msalInstance = new msal.PublicClientApplication(msalConfig);
 
-const loginRequest = {
-    scopes: ["User.Read"]
-};
+async function login() {
 
-async function iniciarLogin() {
+    const loginRequest = {
+        scopes: ["User.Read"]
+    };
 
-    await msalInstance.handleRedirectPromise();
+    try {
 
-    const contas = msalInstance.getAllAccounts();
+        const response = await msalInstance.loginPopup(loginRequest);
 
-    if (contas.length === 0) {
+        console.log("Login efetuado:", response.account);
 
-        console.log("Sem sessão. Redirecionar para login...");
+        mostrarDashboard(response.account);
 
-        await msalInstance.loginRedirect(loginRequest);
+    } catch (error) {
 
-    } else {
-
-        const utilizador = contas[0];
-
-        console.log("Utilizador autenticado:", utilizador);
-
-        sessionStorage.setItem("userName", utilizador.name);
-        sessionStorage.setItem("userEmail", utilizador.username);
+        console.error("Erro no login:", error);
 
     }
 
