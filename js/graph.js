591a90a4e2b39a444d8d680e8aa27ab3ff60b412
jaca000 <almanso.jose@gmail.com>
2026-03-11 14:59:08 +0000

    Update graph.js

--- js/graph.js
+++ js/graph.js
@@ -3,23 +3,13 @@ async function getAccessToken() {
     const account = msalInstance.getAllAccounts()[0];
 
     const request = {
-        scopes: ["User.Read", "Sites.Read.All"],
+        scopes: ["User.Read"],
         account: account
     };
 
-    try {
+    const response = await msalInstance.acquireTokenSilent(request);
 
-        const response = await msalInstance.acquireTokenSilent(request);
-        return response.accessToken;
-
-    } catch (error) {
-
-        console.log("Silent token falhou");
-
-        const response = await msalInstance.acquireTokenPopup(request);
-        return response.accessToken;
-
-    }
+    return response.accessToken;
 
 }
 
