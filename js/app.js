b69973d73df3dfde8c67efdc06d4e948669a7829
jaca000 <almanso.jose@gmail.com>
2026-03-11 14:39:53 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,3 +3,23 @@ console.log("App iniciada");
 document.getElementById("app").innerHTML = `
 <button onclick="login()">Iniciar sessão Microsoft</button>
 `;
+
+function mostrarDashboard(utilizador){
+
+    document.getElementById("app").innerHTML = `
+
+        <h2>Dashboard</h2>
+
+        <p>Bem-vindo ${utilizador.name}</p>
+
+        <button onclick="testar()">Testar ligação</button>
+
+    `;
+
+}
+
+function testar(){
+
+    console.log("Dashboard ativo");
+
+}
