24acac07e82357bf9f88547946c4a1bc2013e63f
jaca000 <almanso.jose@gmail.com>
2026-03-16 12:29:00 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -570,3 +570,43 @@ const diff = venc - hoje;
 return Math.ceil(diff / (1000*60*60*24));
 
 }
+async function carimbarPdf(urlPdf, estado){
+
+const { PDFDocument, rgb, StandardFonts } = PDFLib;
+
+const token = await getAccessToken();
+
+const resp = await fetch(urlPdf,{
+headers:{ Authorization:"Bearer "+token }
+});
+
+const bytes = await resp.arrayBuffer();
+
+const pdfDoc = await PDFDocument.load(bytes);
+
+const pages = pdfDoc.getPages();
+
+const page = pages[0];
+
+const font = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+
+const utilizador = await testarGraph();
+
+const texto =
+estado + "\n" +
+utilizador.displayName + "\n" +
+new Date().toLocaleString("pt-PT");
+
+page.drawText(texto,{
+x:50,
+y:100,
+size:20,
+font:font,
+color: estado==="Aprovado" ? rgb(0,0.6,0) : rgb(0.8,0,0)
+});
+
+const pdfFinal = await pdfDoc.save();
+
+return pdfFinal;
+
+}
