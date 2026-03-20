46f3939935f8f4cd90ac87da6cb9e1ea6006b5b0
jaca000 <almanso.jose@gmail.com>
2026-03-20 11:11:20 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -585,7 +585,7 @@ const { PDFDocument, rgb, StandardFonts } = PDFLib;
 const token = await getAccessToken();
 
 const resp = await fetch(
-`https://graph.microsoft.com/v1.0/sites/montedopasto.sharepoint.com:/sites/AppRegistoFaturas:${new URL(urlPdf).pathname.split("/sites/AppRegistoFaturas")[1]}:/content`,
+`https://graph.microsoft.com/v1.0/sites/montedopasto.sharepoint.com:/sites/AppRegistoFaturas:${new URL(urlPdf).pathname.split(".com")[1]}:/content`,
 {
 headers:{
 Authorization: "Bearer " + token
